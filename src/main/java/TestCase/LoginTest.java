c330166f9c4c33d831bb557adfda3d746c379e30
Kushan <lkkushan@yahoo.com>
2020-02-29 12:30:13 +0530

    committing project

--- src/main/java/TestCase/LoginTest.java
+++ src/main/java/TestCase/LoginTest.java
@@ -1,6 +1,7 @@
 package TestCase;
 import PageClasses.LoginPage;
 import PageClasses.HomePage;
+import Utility.ConstantVariables;
 import Utility.JSONReader;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import java.io.FileNotFoundException;
@@ -10,19 +11,24 @@ import org.json.simple.parser.ParseException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 
 public class LoginTest {
-	
-  
+	public static WebDriver driver  = null;
+@BeforeClass
+public void setup()
+{
+	      driver = BaseClass.initilize();
+ }
+	 
   @Test
   public void  Test () throws FileNotFoundException, IOException, ParseException
   {
+	  		BaseClass.openurl(ConstantVariables.URl);
 	      
-	        WebDriverManager.chromedriver().version("76").setup();
-	        System.setProperty("webdriver.chrome.driver","C:\\chromedriver76\\chromedriver.exe");
-			WebDriver driver = new ChromeDriver();
+
 			LoginPage.using(driver).launch().setUserName(JSONReader.ReadJSONFile("User_Name", "./Data/data.json")).setPassword(JSONReader.ReadJSONFile("Password", "./Data/data.json")).clickLogin();
      		Assert.assertTrue(HomePage.using(driver).getHomePageDashboardUserName());
      		driver.close();
